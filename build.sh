acfa2ef14e960f2cdd84e8b026f52de7b268f737
chainomi <72167910+chainomi@users.noreply.github.com>
2021-03-15 15:23:53 -0400

    3rd commit

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -xe
 echo "copying env files"
 
 cp .env.prod .env
-cp .env.prod /home/ec2-user/Docker_env_demo/docker/.env
+cp .env.prod ./docker/.env
 
 echo "Loading the .env files: \n" "info"
 export $(grep -v '^#' docker/.env | xargs -0)
