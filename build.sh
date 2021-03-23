c80e1ff208f3ee666a2dce8c259ab3ed6d0b9dd8
chainomi <72167910+chainomi@users.noreply.github.com>
2021-03-23 18:09:11 -0400

    update to build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,16 @@ export $(grep -v '^#' docker/.env | xargs -0)
 export $(grep -v '^#' .env | xargs -0)
 
 docker-compose build
-docker-compose up -d
-docker-compose images
-docker-compose logs
+echo "loging into AWS"
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 488144151286.dkr.ecr.us-east-2.amazonaws.com
+
+echo "tagging image for AWS ECR"
+docker tag ${MY_IMAGE_NAME}:latest 488144151286.dkr.ecr.us-east-2.amazonaws.com/tomcat_env_demo:latest
+
+echo "pushing image to AWS ECR"
+
+docker push 488144151286.dkr.ecr.us-east-2.amazonaws.com/tomcat_env_demo:latest
+
+#docker-compose up -d
+#docker-compose images
+#docker-compose logs
